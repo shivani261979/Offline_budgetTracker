311319983ac94486e6613ada89b1eb3996f3b38a
shivani jha <shivani261979@gmail.com>
2020-07-26 01:29:48 -0400

    fixed a bug

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const compression = require("compression");
 
 const PORT = (process.env.PORT || 3000);
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
